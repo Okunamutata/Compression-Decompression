8e4534f690e07004f0000729b3547f845f3dfffc
fbinateeq <44850986+fbinateeq@users.noreply.github.com>
2019-04-07 00:18:22 -0700

    Add files via upload

--- Dictionary.java
+++ Dictionary.java
@@ -1,27 +1,64 @@
 
-import java.util.ArrayList;
- 
-import List.Node;
-
 public class Dictionary{
-    ArrayList<Node> table;
-    public int ts; //table size
     
-    public Dictionary(int size){ // does Int works?
-       
-        table = new ArrayList<Node>(nextPrime(size));
+    LinkedList[] theArray;
+    Node head;
+    int arraySize;
+    int itemsInArray;
+    int code; // current code of the dictionary    example:  aa --> 128 
+    public static final int  ARRAYSIZE = nextPrime(1000);
+    /*
+    * Constructor for Dictionary object
+    * @param int size, the size of the hashTable
+    */ 
+    public Dictionary()
+    {
+    	arraySize = nextPrime(ARRAYSIZE);
         
-        for(int i = 0; i < table.length; i++){
-            table.add(new LinkedList<Node>());
-
-           
+        for(int i = 0; i < arraySize; i++)
+        {
+            theArray[i] = new LinkedList();
         }
-       
-
         
     }
+    public Dictionary(int newArraySize)
+    {
+    	arraySize = newArraySize;
+        
+        for(int i = 0; i < arraySize; i++)
+        {
+            theArray[i] = new LinkedList();
+        }
+        
+    }
+    public void intzDic()
+    {
+    	char c;
+    	String str;
+        for(code = 32 ; code <= 126; code++)
+        {
+            //complete initializing dictionary with ASCII 
+        	c=(char)code;
+        	str= Character.toString(c);
+        	insert(str);
+        }
+    }
+    public void insert(String str)
+    {
+        Node newNode = new Node(str, code );
 
-
+        //calculate hashkey
+    	int hashKey  = hashFunc(str);
+        LinkedList whichList = theArray[hashFunc(str)];
+        if(!whichList.ListContains(str))
+        {
+        	whichList.ListInsertEnd(str, code);
+        	itemsInArray++;
+        	//if(averageLinkedList() > 10 )
+        	// rehash();
+        }
+        
+    }
     public static int nextPrime(int n){
         if (n % 2 == 0)
             n++;
@@ -30,8 +67,6 @@ public class Dictionary{
         return n;
 
     }
-
-    
     public static boolean isPrime(int n ){
         if (n == 2 || n == 3){ 
             return true;
@@ -47,53 +82,61 @@ public class Dictionary{
         return true;
        
     }
-
-    public void rehash(){
-
-    }
-    
-    public void intzDic(){
-        for(int i = 32; i < 126; i++){
-            int hash = hashCode(i, 191);
-            //complete initializeing dicitionary wth ascii 
-
-        }
-    }
-
-
-    public void makeEmpty(){
-        for(int i = 0; i < table.length; i++){
-            table.remove(i);
-
-             //why save table size?
-             ts = 0;
-        }
-    }
-    public int hashCode(int asciiVal, int ts){
-        int index;
-        index = (int) (asciiVal * 31) % ts;
-        return index;
-    }
-
-    public void insert(int index ){
-        short tableHead;
-        Node newNode = new Node((ascii) index);
-        tableHead = dictionary[index];
-        if(table.head.getNext() == null){
-            head.setNext(newNode());
-        }
-        else{
-            Node curr = head;
-                if(curr.getNext() == null){
-                    curr.setNext(newNode()); 
-                }
+    //Methos to test hashtable and rehash
+    private void displayArray(){
+        Node curr;
+        int listLength;
+        LinkedList list;
+        for(int i  = 0; i < this.arraySize-1; i++){
+            System.out.println("the Array index " + i);
+            list = theArray[i];
+            //listLength = theArray[i].listLength();
             
+        }    
+    }
+    public Dictionary Rehash()
+    {
+        int oldArrSize = theArray.length;
+        LinkedList [] oldArr = theArray;
+        
+        // change the array size
+        int newArrSize = nextPrime(oldArrSize * 2);
+        // create a new table and initialize it
+        Dictionary newTable = new Dictionary(newArrSize);
+        intzDic();
+        
+        // copy the table to the new table
+        for( int i = 0; i < oldArrSize ; i++  )
+        {
+        	String str;
+        	for(int j = 0; j < oldArr[i].listLength() ; i++)
+        	{
+        		str = oldArr[i].ListRetrieveString(j) ;
+        		insert(str);
+        	}
+        	
         }
+        
+		return newTable;
     }
-
-
-
-    public static void main(String[] args){
-       
+    /*
+    * Creates key value for hash table insertion
+    * @parama String str, the ctring to be encoded
+    */
+    public int hashFunc(String str)
+    {
+        int hashVal = 0;
+        
+        for( int i = 0; i < str.length(); i++)
+        	hashVal = 37 * hashVal + str.charAt(i) ;
+        
+        hashVal %= theArray.length;
+        if(hashVal < 0 )
+        	hashVal += theArray.length;
+        
+        return hashVal;
     }
+   
+   
+  
 }
--- Node.java
+++ Node.java
@@ -1,39 +1,55 @@
-package List;
+
 
 public class Node {
-	private Object item;
-	private Node next;
 	
+	private String str;
+	private Node next;
+	private int code;
 	
 	//Default constructor or a node
-	public Node(Object newItem)
+	public Node(String newStr, int newCode)
 	{
-		item = newItem;
+		String strVal = str;
+		code = newCode;
 		next = null;
 	}
-	
 	//constructor for item and node
-	public Node(Object newItem, Node newNode)
+	public Node(String str, int newCode, Node newNode)
 	{
-		item = newItem; 
+		String strVal = str;
+		code = newCode;
 		next = newNode;
 	}
-	
-	public Object getItem()
-	{
-		return item;
+	public Node getNode()
+	{ 
+		return this;
 	}
-	
-	
+	public int getCode()
+	{
+		return this.code;
+	}
+	public String getString()
+	{
+		return this.str;
+	}
 	public Node getNext()
 	{
 		return next;
 	}
-	
-	
 	public void setNext(Node newNode)
 	{
 		next = newNode;
 	}
-	
+	public void setString(String newStr) 
+	{
+		str = newStr;
+	}
+	public void setCode(int newCode) {
+		code = newCode;
+	}
+	public void setBoth(String newStr, int newKey) 
+	{
+		str = newStr;
+		code= newKey;
+	}
 }
